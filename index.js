79daa436052ee9275fd7f0a50cd59f917721e5c8
Rick Nilon <r.nilon92@gmail.com>
2017-12-18 19:55:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function dwarfRollCall(dwarves) {
+  var newArr = [];
+
+  for(var i = 0; i < dwarves.length; i++){
+    var num = i + 1;
+    newArr.push(`${num}. ${dwarves[i]}`)
+  }
+  return newArr.join(" ")
 }
 
 function summonCaptainPlanet(planeteerCalls){
